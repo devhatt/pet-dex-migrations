52a0311681aff99d668cfb3b75a1ec746a96c596
k1nha <lucascmpus@gmail.com>
2024-04-12 20:40:34 -0300

    feat: create seed with faker js

--- prisma/breeds/index.ts
+++ prisma/breeds/index.ts
@@ -1,36 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 import { dogs } from "./dogs";
+import { cats } from "./cats";
 
 const prismaClient = new PrismaClient();
 
 async function seedBreeds() {
-  await Promise.all(
+  await Promise.all([
     dogs.map(async (dog) => {
       return prismaClient.breed.create({
         data: {
           name: dog.nome,
-          img_url: dog.imgUrl,
-          description: dog.descricao,
-          height: dog.altura,
-          weight: dog.peso,
-          size: dog.tamanho,
-          life_expectancy: dog.esperancaDeVida,
           type_of: "cachorro",
-          physical_char: dog.caracteristicasFisicas.join(","),
-          coat: dog.tipoDePelo.join(","),
-          category: dog.FCI,
-          ideal_for: dog.idealPara.join(","),
-          training_level: dog.nivelDeAdestramento,
-          disposition: dog.carater.join(","),
-          housing_structures: dog.clima,
+          size: dog.tamanho || "N/A",
+          description: dog.descricao,
+          height: dog.altura || "N/A",
+          weight: dog.peso || "N/A",
+          physical_char: dog.caracteristicasFisicas.join(",") || "N/A",
+          disposition: dog.carater.join(",") || "N/A",
+          img_url: dog.imgUrl,
+          life_expectancy: dog.esperancaDeVida || "N/A",
+          coat: dog.tipoDePelo.join(",") || "N/A",
+          category: dog.FCI || "N/A",
+          ideal_for: dog.idealPara.join(",") || "N/A",
+          training_level: dog.nivelDeAdestramento || "N/A",
+          housing_structures: dog.clima || "N/A",
         },
       });
-    })
-  );
+    }),
+
+    cats.map(async (cat) => {
+      return prismaClient.breed.create({
+        data: {
+          name: cat.nome,
+          size: cat.tamanho || "N/A",
+          img_url: cat.imgUrl,
+          description: cat.descricao || "N/A",
+          height: "N/A",
+          weight: cat.peso || "N/A",
+          life_expectancy: cat.esperancaDeVida || "N/A",
+          type_of: "gato",
+          physical_char: cat.caracteristicasFisicas.join(",") || "N/A",
+          coat: cat.tipoDePelo.join(",") || "N/A",
+          category: cat.FIFE || "N/A",
+          disposition: cat.carater.join(",") || "N/A",
+          housing_structures: cat.clima || "N/A",
+        },
+      });
+    }),
+  ]);
 }
 
 seedBreeds()
   .then(async () => {
+    console.log("Breeds seeded successfully");
     await prismaClient.$disconnect();
   })
   .catch(async (err) => {
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,50 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prismaClient = new PrismaClient();
 
-async function seed() {}
+async function seed() {
+  await prismaClient.user.create({
+    data: {
+      account_type: "user",
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      name: faker.person.firstName(),
+      address: {
+        create: {
+          city: faker.location.city(),
+          state: faker.location.state(),
+          address: faker.location.streetAddress(),
+          latitude: faker.location.latitude(),
+          longitude: faker.location.longitude(),
+        },
+      },
+      avatar_url: faker.image.avatar(),
+      birthdate: faker.date.past(),
+      document_id: faker.number.int({ max: 1000, min: 1 }).toString(),
+      phone_number: "1234567890",
+      pets: {
+        create: {
+          adoption_date: faker.date.past(),
+          birthdate: faker.date.past(),
+          name: faker.animal.dog(),
+          size: "small",
+          weight: 10,
+          breed_id: 1, // Talvez seja necessário mudar de acordo com sua necessidade
+          available_to_adoption: false,
+          neutered: true,
+          vaccines: {
+            create: {
+              date: faker.date.past(),
+              name: "V8",
+              doctor_id: "1",
+            },
+          },
+        },
+      },
+    },
+  });
+}
 
 seed()
   .then(async () => {
